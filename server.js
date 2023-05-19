579adc571dde14df64a56cc4beb35113e3b23a56
Allan Robert Tan <argt@duck.com>
2023-05-19 09:47:07 +0800

    add MONGODB_URI

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config()
 }
 
-const PORT = process.env.PORT || 3000
-
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const PORT = process.env.PORT || 3000
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connected to the DB successfully')
+
+    app.listen(PORT, () => {
+      console.log(`Server now listening on port: ${PORT}...`)
+    })
+  })
+
 app.get('/', (req, res) => {
   res.send('HEY')
-})
-
-app.listen(PORT, () => {
-  console.log('now listening on port ' + PORT)
 })
