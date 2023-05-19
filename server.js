3a35f66d69c91b990ee9a78003ce40af60d58c0a
Allan Robert Tan <argt@duck.com>
2023-05-19 13:06:35 +0800

    add view engine and set structQuery to false

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+mongoose.set('strictQuery', false)
 
 const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI
