3751543e16fdcca065b6a6186201e51eeafc81d0
Allan Robert Tan <argt@duck.com>
2023-05-21 20:51:03 +0800

    add index page route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 import { Router } from 'express'
-import { get_short_url } from '../controllers/get-short-url.js'
+import { get_index_page, get_short_url } from '../controllers/get-short-url.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.sendFile('../public/index.html')
-})
-
+router.get('/', get_index_page)
 router.get('/:short_url', get_short_url)
 
 export default router
